0c440676a554c7d25c97032a9e4cf1ed7c63be07
Ryan Johnson <ryan@258webco.com>
2020-05-13 17:06:44 -0500

    Develop Contact  page component.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -59,8 +59,8 @@ const menuItems = [
   },
   {
     listIcon: <ContactMail />,
-    listText: "Contacts",
-    listPath: "/contacts",
+    listText: "Contact",
+    listPath: "/contact",
   },
 ];
 
